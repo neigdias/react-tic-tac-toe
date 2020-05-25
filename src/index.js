0c971d13818aa3bb6d13d24922161a00654a244d
Nei <neigdias@gmail.com>
2020-05-25 13:14:38 -0300

    function to calculate the winner

--- src/index.js
+++ src/index.js
@@ -9,7 +9,27 @@ import './index.css';
         </button>
       );
   }
-  
+
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }  
+
   class Board extends React.Component {
     constructor(props){
         super(props);
@@ -37,7 +57,13 @@ import './index.css';
     }
   
     render() {
-      const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+      const winner = calculateWinner(this.state.squares);
+      let status;
+      if (winner){
+        status = 'Winner is : ' + winner;
+      } else {
+        status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+      }
   
       return (
         <div>
